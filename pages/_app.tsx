867fbd891bb92ab7628a22b7f038c45a14e4cbe7
Parallyz <vladislav.verus@gmail.com>
2022-11-03 15:34:17 +0200

    added sass

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import "../styles/globals.css";
+import "../styles/globals.scss";
 
 import * as React from "react";
 
